d639a5987b28a9af251d86af94df801adcbb600a
sspat <sspat@sona>
2022-10-20 11:05:22 +0530

    First Commit...

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+       public static void main(String[] args) {
+    	   String hungry = "yes";
+    	   if(hungry == "yes") {
+    		   System.out.println("Eat something");
+    	   }else {
+    		   System.out.println("Do work");
+    	   }
+       }
+}
